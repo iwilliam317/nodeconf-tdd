9debfcb25799bff1f623c52fc956c70d6293ac0b
william <willshinji@gmail.com>
2018-09-17 13:09:01 -0300

    create server

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports.soma = (a, b) => a + b;
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+  host: 'localhost',
+  port: '3000'
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, h){
+    return 'Hello World from Hapi'
+  }
+});
+
+async function start() {
+  try{
+    await server.start();
+  }
+  catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log('Server running at:', server.info.uri);
+}
+
+start();
