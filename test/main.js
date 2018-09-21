e3bd7e4b882d8857e1d02e17a21704e170c5932c
william <willshinji@gmail.com>
2018-09-21 16:39:23 -0300

    add first test

--- test/main.js
+++ test/main.js
@@ -1,9 +1,17 @@
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const assert = require('assert');
+const server = require('../index');
 
 lab.experiment('GET /', () => {
   lab.test('should return "Hello World from Hapi"', () => {
-    assert(true);
+    server.inject({
+      url: '/',
+      method: 'GET'
+    })
+      .then((res) => {
+        assert.equal(res.statusCode, 200);
+        assert.equal(res.result, 'Hello World from Hapi');
+      })      
   })
 })
