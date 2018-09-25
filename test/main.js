7d94e5254c70fa97c073a7ce1fd5a2d7726e5c97
william <willshinji@gmail.com>
2018-09-25 19:49:31 -0300

    refactor test

--- test/main.js
+++ test/main.js
@@ -4,14 +4,10 @@ const assert = require('assert');
 const server = require('../index');
 
 lab.experiment('GET /', () => {
-  lab.test('should return "Hello World from Hapi"', () => {
-    server.inject({
-      url: '/',
-      method: 'GET'
-    })
-      .then((res) => {
-        assert.equal(res.statusCode, 200);
-        assert.equal(res.result, 'Hello World from Hapi');
-      })      
+  lab.test('should return "Hello World from Hapi"', async () => {
+    let res = await server.inject({ url: '/',method: 'GET' });
+    assert.equal(res.statusCode, 200);
+    assert.equal(res.result, 'Hello World from Hapi');
+
   })
 })
